527916a0062e1fdf3742bbf30ef098a9d5f8bee5
matpl2 <70451479+matpl2@users.noreply.github.com>
2021-02-14 00:49:16 +0100

    Update queries.py

--- queries.py
+++ queries.py
@@ -5,15 +5,15 @@ import configparser
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
+# CREATE SCHEMAS
+create_schema_data = ("""CREATE SCHEMA data;""")
+create_schema_wbr = ("""CREATE SCHEMA wbr;""")  
+
 # DROP TABLES
 table_fligts_drop = "DROP TABLE IF EXISTS data.flights"
 table_airports_drop = "DROP TABLE IF EXISTS data.airports"
 table_airlines_drop = "DROP TABLE IF EXISTS data.airlines"
 
-
-# CREATE SCHEMAS
-create_schema_data = ("""CREATE SCHEMA data;""")
-create_schema_wbr = ("""CREATE SCHEMA wbr;""")  
 
 # CREATE TABLES
 table_fligths_create = ("""
@@ -58,41 +58,23 @@ table_fligths_create = ("""
 table_airlines_create = ("""
     CREATE TABLE IF NOT EXISTS data.airlines
     (
-        YEAR VARCHAR(6)
-        ,MONTH VARCHAR(4)
-        ,DAY VARCHAR(4)
-        ,DAY_OF_WEEK VARCHAR(4)
-        ,AIRLINE VARCHAR(4)
-        ,FLIGHT_NUMBER VARCHAR(8)
-        ,TAIL_NUMBER  VARCHAR(14)
-        ,ORIGIN_AIRPORT VARCHAR(12)
-        ,DESTINATION_AIRPORT VARCHAR(12)
-        ,SCHEDULED_DEPARTURE VARCHAR(8)
-        ,DEPARTURE_TIME VARCHAR(8)
-        ,DEPARTURE_DELAY INT
-        ,TAXI_OUT SMALLINT
-        ,WHEELS_OFF VARCHAR(8)
-        ,SCHEDULED_TIME VARCHAR(8)
-        ,ELAPSED_TIME VARCHAR(8)
-        ,AIR_TIME VARCHAR(8)
-        ,DISTANCE INT
-        ,WHEELS_ON VARCHAR(8)
-        ,TAXI_IN VARCHAR(8)
-        ,SCHEDULED_ARRIVAL VARCHAR(8)
-        ,ARRIVAL_TIME VARCHAR(8)
-        ,ARRIVAL_DELAY INT
-        ,DIVERTED VARCHAR(2)
-        ,CANCELLED VARCHAR(2)
-        ,CANCELLATION_REASON VARCHAR(2)
-        ,AIR_SYSTEM_DELAY INT
-        ,SECURITY_DELAY INT
-        ,AIRLINE_DELAY INT
-        ,LATE_AIRCRAFT_DELAY INT
-        ,WEATHER_DELAY INT
+        IATA_CODE	VARCHAR (4)
+        ,AIRLINE VARCHAR (144)
+        ,primary key(IATA_CODE)
     )
-    DISTSTYLE EVEN
-    SORTKEY (AIRLINE);
+    DISTSTYLE ALL;  
 """)
+
+table_airports_create = ("""
+    CREATE TABLE IF NOT EXISTS data.airports
+    (
+        IATA_CODE VARCHAR(4)
+        ,AIRLINE VARCHAR (144)
+        ,primary key(IATA_CODE)
+    )
+    DISTSTYLE ALL;  
+""")
+
 
 # LOADING TABLE
 table_flights_copy = ("""
@@ -108,6 +90,20 @@ table_flights_copy = ("""
     config['CLUSTER']['REGION']
 )
 
+table_airlines_copy = ("""
+    COPY {} FROM {}
+    IAM_ROLE '{}'
+    delimiter ','
+    IGNOREHEADER 1
+    region '{}';
+""").format(
+    'data.airlines',
+    config['S3']['DATAAIRL'],
+    config['IAM_ROLE']['ARN'],
+    config['CLUSTER']['REGION']
+
+
+
 # TRUNCATE TABLE
 table_flights_truncate = ("""
     TRUNCATE data.flights;
@@ -118,3 +114,9 @@ table_flights_truncate = ("""
 
 queries_flight = [table_fligths_create, table_flights_truncate, table_flights_copy]
 
+table_airlines_drop
+table_airports_drop
+table_airlines_create 
+
+table_airlines_create 
+table_airports_create
